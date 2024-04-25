905354c44c1f04d78f49c13642d604c8c7af7303
harsharajchalla <160366531+harsharajchalla@users.noreply.github.com>
2024-04-24 21:52:32 -0400

    Update 05-04-2024.R

--- 05-04-2024.R
+++ 05-04-2024.R
@@ -98,34 +98,6 @@ result | result2
 result & result2
 isTRUE(result)
 
-#Assignment1
-
-#Harsharaj Challa
-# Define the numbers
-numbers <- c(7, 12, 5)
-
-# Calculate the average
-average <- mean(numbers)
-
-# Print the average
-print(paste("The average of the numbers is:", average))
-
-first_name <- "Harsharaj"
-# Print the welcome message
-print(paste("Welcome to R,",first_name))
-
-# Define the variables
-x <- 7
-y <- 12
-z <- 4
-
-# Check if x is bigger than y and z
-if (x > y) {
-  print("x is bigger than y")
-} else {
-  print("x is not bigger than y")
-}
-
 if (x > z) {
   print("x is bigger than z")
 } else {
